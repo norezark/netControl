66ad990359841c55cf8b71a866fd0285ac7d5388
reon <reon111213@gmail.com>
2018-02-27 05:55:20 +0900

    Revert "ミス"
    This reverts commit 733831ff394cb1094447966e0040a5e02f33f83a.

--- gui/graphical.cs
+++ gui/graphical.cs
@@ -1,6 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace netControl
 {
