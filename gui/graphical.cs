c5fb3c1d4dfbf81f783e563e7ecf26d83a0dab9b
reon <reon111213@gmail.com>
2018-02-27 05:54:21 +0900

    Revert "ミス"
    This reverts commit e2f9c4b4c26cfc47167034aa56017b192c4137b7.

---
+++ gui/graphical.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace netControl
+{
+    public partial class Graphical: Form
+    {
+        public Graphical()
+        {
+            InitializeComponent();
+        }
+
+        private List<double> data = new List<double>();
+
+        public void AddData(double p)
+        {
+            data.Add(p);
+            if (data.Count == 11) data.RemoveAt(0);
+            chart1.Series[0].Points.Clear();
+            for(int i = 0; i < data.Count; i++)
+            {
+                chart1.Series[0].Points.AddXY(i+1, data[i]);
+            }
+        }
+
+        private Point mousePoint;
+
+        private void Graphical_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                mousePoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void Graphical_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                Left += e.X - mousePoint.X;
+                Top += e.Y - mousePoint.Y;
+            }
+        }
+    }
+}
